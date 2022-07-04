ac588961535ddbadd135bad1ef44a06d58896582
rapahel-andres <rapahel.andres@icloud.com>
2022-07-03 19:20:08 -0500

    Se cambio el metodo de setUp a publico

--- src/test/java/com/example/fibonacci/services/FibonacciServiceImplTest.java
+++ src/test/java/com/example/fibonacci/services/FibonacciServiceImplTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FibonacciServiceImplTest {
 
     @BeforeEach
-    private void setUp() {
+    public void setUp() {
         MockitoAnnotations.openMocks(this);
     }
 
